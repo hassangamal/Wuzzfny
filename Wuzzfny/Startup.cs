b5a27509f57fd497fba06c150d310d2b98ca5c0c
hassangamal <gamalhassan807@yahoo.com>
2018-05-06 02:05:12 +0200

    nav and footer

---
+++ Wuzzfny/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wuzzfny.Startup))]
+namespace Wuzzfny
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
